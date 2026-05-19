f56d72e96b0a927c244119a72986e067cfdd483c
Sohrab Alam <sohrab.alam86@gmail.com>
2026-05-19 22:46:57 +0530

    Programs.

--- ShareTradingService/src/main/java/com/management/weight/controller/ShareTradingController.java
+++ ShareTradingService/src/main/java/com/management/weight/controller/ShareTradingController.java
@@ -5,11 +5,9 @@ import java.math.BigDecimal;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.management.weight.helper.ShareTradingHelper;
@@ -45,10 +43,18 @@ public class ShareTradingController {
 		return new ResponseEntity<>(share, HttpStatus.OK);
 	}
 
-	@GetMapping(ShareTradingContants.SELLING_DETAILS)
-	public ResponseEntity<SellingShare> getSellingDetails(@RequestParam BigDecimal lastTradePrice,
-			@RequestParam BigDecimal quantity, @RequestParam BigDecimal averageCost,
-			@RequestParam BigDecimal investedAmount, @RequestParam int holdingDurationInMonths) throws Exception {
+	@PostMapping(ShareTradingContants.SELLING_DETAILS)
+	public ResponseEntity<SellingShare> getSellingDetails(@RequestBody SellingShare sellingShare) throws Exception {
+
+		BigDecimal lastTradePrice = sellingShare.getLastTradePrice();
+
+		BigDecimal quantity = sellingShare.getQuantity();
+
+		BigDecimal averageCost = sellingShare.getAverageCost();
+
+		BigDecimal investedAmount = sellingShare.getInvestedAmount();
+
+		int holdingDurationInMonths = sellingShare.getHoldingDurationInMonths();
 
 		BigDecimal totalSharesPrice = ShareTradingUtils.multiply(lastTradePrice, quantity);
 
